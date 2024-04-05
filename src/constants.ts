cfb3183c2d559ca1250295c8d6f136ee09b17f0d
Fadel Drissi Toubbali <fadel.toubbali@coolbet.com>
2024-04-05 06:34:31 +0300

    refact: separate game options constant

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+import { E_POSITION, TPosition } from "./types";
+
+export const POSITIONS: TPosition[] = [
+    {
+      color: 'red',
+      name: E_POSITION.SCISSORS,
+    },
+    {
+      color: 'green',
+      name: E_POSITION.PAPER,
+    },
+    {
+      color: 'blue',
+      name: E_POSITION.ROCK,
+    },
+  ];
