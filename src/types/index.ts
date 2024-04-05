84336d813682d5f166cf621abb244aa6046e16cd
Fadel Drissi Toubbali <fadel.toubbali@coolbet.com>
2024-04-05 06:30:08 +0300

    feat: add main types and enums

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,45 @@
-export enum EPOSITION {
-    ROCK = 'rock',
-    SCISSORS = 'scissors',
-    PAPER = 'paper',
+export enum E_POSITION {
+  ROCK = 'rock',
+  SCISSORS = 'scissors',
+  PAPER = 'paper',
+}
+
+export enum E_STATUS {
+  WON = 'won',
+  LOST = 'lost',
+  TIE = 'tie'
+}
+
+export type TPosition = {
+  color: 'red' | 'green' | 'blue';
+  name: E_POSITION;
+};
+
+export type TGameConfig = {
+  baseBalance: number;
+  maxAllowedNumberOfSelections: number;
+  minStake: number;
+  stakeIncrementor: number;
+  odds: [number, number];
+  tieOdds: [number, number];
+};
+
+export type TBet = {
+  selection: E_POSITION;
+  stake: number;
+};
+
+export type TTicket = {
+  results: TResult[];
+  totalStake: number;
+  wonAmount: number;
+};
+
+export type TResult = {
+  playerSelection: E_POSITION;
+  stake: number;
+  status: E_STATUS;
+  odd: number;
+  amount: number;
+  computerSelection: E_POSITION;
 }
